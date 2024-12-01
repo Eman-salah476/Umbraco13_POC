1e1cb2c1daabe452c186f4a6581b93edc3459124
Eman Salah <Eman.Salah@linkdev.com>
2024-12-01 17:18:21 +0200

    Implement Custom Filters

---
+++ LinkDev.GAFI.EgyptReady.Web/Composers/CustomFilterComposer.cs
@@ -0,0 +1,19 @@
+﻿using LinkDev.GAFI.EgyptReady.Web.Handlers.BlogsCategoryHandlers;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DeliveryApi;
+
+namespace LinkDev.GAFI.EgyptReady.Web.Composers
+{
+    public class CustomFilterComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            // Register the custom filter handler
+            builder.Services.AddSingleton<IFilterHandler, BlogCategoryFilterHandler>();
+
+            // Register the custom index handler
+            builder.Services.AddSingleton<IContentIndexHandler, BlogCategoryIndexHandler>();
+
+        }
+    }
+}
---
+++ LinkDev.GAFI.EgyptReady.Web/Handlers/BlogsCategoryHandlers/BlogCategoryFilterHandler.cs
@@ -0,0 +1,35 @@
+﻿using Umbraco.Cms.Core.DeliveryApi;
+
+namespace LinkDev.GAFI.EgyptReady.Web.Handlers.BlogsCategoryHandlers
+{
+    public class BlogCategoryFilterHandler : IFilterHandler
+    {
+
+        private const string CategorySpecifier = "category:";
+        private const string FieldName = "blogCategory";
+
+
+        public bool CanHandle(string query)
+        {
+            // Check if the query starts with the specified prefix
+            return query.StartsWith(CategorySpecifier, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public FilterOption BuildFilterOption(string filter)
+        {
+            // Extract the category ID from the filter query
+            var categoryId = filter.Substring(CategorySpecifier.Length);
+
+            // There might be several values for the filter
+            var values = categoryId.Split(',');
+
+            return new FilterOption
+            {
+                FieldName = FieldName,
+                Values = values,
+                Operator = FilterOperation.Is
+            };
+        }
+     
+    }
+}
