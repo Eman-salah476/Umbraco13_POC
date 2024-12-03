7ae16a9d2a990aaa6a1668515e03a5f6c5317637
Eman Salah <Eman.Salah@linkdev.com>
2024-12-03 10:55:35 +0200

    Configured dateHandler

--- LinkDev.GAFI.EgyptReady.Web/Composers/CustomFilterComposer.cs
+++ LinkDev.GAFI.EgyptReady.Web/Composers/CustomFilterComposer.cs
@@ -6,11 +6,16 @@ namespace LinkDev.GAFI.EgyptReady.Web.Composers
 {
     public class CustomFilterComposer : IComposer
     {
+        /// <summary>
+        /// We register handler and indexer classes in the Startup or Composer file to ensure that Umbraco's Dependency Injection (DI) container
+        /// is aware of these classes and can use them during the Content Delivery API's runtime operations. 
+        /// </summary>
         public void Compose(IUmbracoBuilder builder)
         {
             // Register the custom filter handler
             builder.Services.AddSingleton<IFilterHandler, BlogCategoryFilterHandler>();
             builder.Services.AddSingleton<IFilterHandler, BlogSearchFilterHandler>();
+            builder.Services.AddSingleton<IFilterHandler, BlogDateFilterHandler>();
 
             // Register the custom index handler
             builder.Services.AddSingleton<IContentIndexHandler, BlogIndexHandler>();
