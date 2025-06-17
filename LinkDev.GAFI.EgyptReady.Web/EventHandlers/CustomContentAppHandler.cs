5e23fe9faa4961b9bb4b96dff3aa62329a0425a4
Eman Salah <emanelzoghby1579@gmail.com>
2025-06-17 14:37:50 +0300

    Added setting to enable and disable the feature and limit fetaure to specific user groups

---
+++ LinkDev.GAFI.EgyptReady.Web/EventHandlers/CustomContentAppHandler.cs
@@ -0,0 +1,46 @@
+﻿using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.Notifications;
+using Umbraco.Cms.Core.Security;
+
+namespace LinkDev.GAFI.EgyptReady.Web.EventHandlers
+{
+    public class CustomContentAppHandler : INotificationHandler<SendingContentNotification>
+    {
+        private readonly IBackOfficeSecurity _backOfficeSecurity;
+        private readonly IConfiguration _configuration;
+        string uRLInspectionContentAppAlias = "urlInspectionTool";
+        string AllowedUserGroupAliases;
+        bool isEnabled;
+          
+        public CustomContentAppHandler(IBackOfficeSecurity backOfficeSecurity, IConfiguration configuration)
+        {
+            _backOfficeSecurity = backOfficeSecurity;
+            _configuration = configuration;
+            AllowedUserGroupAliases = _configuration.GetValue<string>("Umbraco:CMS:Integrations:SEO:GoogleSearchConsole:Settings:AllowedUserGroupAliases") ?? string.Empty;
+            isEnabled = _configuration.GetValue<bool>("Umbraco:CMS:Integrations:SEO:GoogleSearchConsole:Settings:Enabled");
+
+        }
+
+        public void Handle(SendingContentNotification notification)
+        {
+            if (!isEnabled)
+            {
+                notification.Content.ContentApps = notification.Content.ContentApps.Where(x => x.Alias != uRLInspectionContentAppAlias);
+                return;
+            }
+
+            var userGroupeAliases = AllowedUserGroupAliases.Split(',')
+                .Select(x => x.Trim())
+                .Where(x => !string.IsNullOrEmpty(x))
+                .ToList();
+
+            var currentUser = _backOfficeSecurity.CurrentUser;
+            if (currentUser is not null && userGroupeAliases.Any() &&
+                currentUser.Groups.Any(x => userGroupeAliases.Any(a => a.ToLower() == x.Alias.ToLower())))
+                return;
+
+            notification.Content.ContentApps = notification.Content.ContentApps.Where(x => x.Alias != uRLInspectionContentAppAlias);
+            
+        }
+    }
+}
